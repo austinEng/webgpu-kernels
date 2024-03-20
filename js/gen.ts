d24a02c5c5083fbf655f788885fd4c1486f29d52
Austin Eng <enga@chromium.org>
2024-03-20 14:35:09 -0700

    Don't use hashes in wgsl shader file names. Too hard to read

--- js/gen.ts
+++ js/gen.ts
@@ -9,21 +9,23 @@ for (const file of await fs.readdir(new URL('../data/', import.meta.url))) {
 const shaders = new Map<string, string>();
 
 for (const [name, b] of Object.entries(benchmarks)) {
+  let shortNameCount = 0;
   for (const [case_name, params] of Object.entries(b.cases)) {
     const { wgsl, requiredLimits, requiredFeatures } = b.generateTest(params);
-    let hexCode = shaders.get(wgsl);
-    if (hexCode === undefined) {
-      const hashArray = Array.from(new Uint8Array(await subtle.digest('SHA-1', Buffer.from(wgsl))));
-      hexCode = hashArray
-        .map((b) => b.toString(16).padStart(2, "0"))
-        .join("");
-      fs.writeFile(new URL('../data/' + hexCode + '.wgsl', import.meta.url), wgsl);
-      shaders.set(wgsl, hexCode);
+    let shortName = shaders.get(wgsl);
+    if (shortName === undefined) {
+      shortName = name + '_' + (shortNameCount ++);
+      // const hashArray = Array.from(new Uint8Array(await subtle.digest('SHA-1', Buffer.from(wgsl))));
+      // shortName = hashArray
+      //   .map((b) => b.toString(16).padStart(2, "0"))
+      //   .join("");
+      fs.writeFile(new URL('../data/' + shortName + '.wgsl', import.meta.url), wgsl);
+      shaders.set(wgsl, shortName);
     }
 
     fs.writeFile(new URL('../data/' + name + '_' + case_name + '.json', import.meta.url),
                  JSON.stringify({
-                   shader: `${hexCode}.wgsl`,
+                   shader: `${shortName}.wgsl`,
                    requiredFeatures,
                    requiredLimits,
                    params,
