8d374b4433d93d9318a53d75b2fdbbd825dc75f7
austinEng <2154796+austinEng@users.noreply.github.com>
2024-03-20 19:34:39 +0000

    Deploying to gh-pages from  @ 13680769f21f0ba17f21899cbc7bbcc9663fd1a5 🚀

--- bundle.js
+++ bundle.js
@@ -846,12 +846,14 @@ function addSort(th) {
     }));
 }
 const queryParams = new URLSearchParams(window.location.search);
-for (const [name, b] of Object.entries(benchmarks)) {
+for (const [name,] of Object.entries(benchmarks)) {
     const link = document.createElement('a');
     link.innerText = name;
     link.href = `./?benchmark=${name}`;
     link.classList.add('benchmark-link');
     document.body.appendChild(link);
+}
+for (const [name, b] of Object.entries(benchmarks)) {
     if (name !== queryParams.get('benchmark')) {
         continue;
     }
